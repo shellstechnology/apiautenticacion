da618dda2ddb0f22d219bf5acb1ae307ab316055
Matias Meneses <mat.meneses47@gmail.com>
2023-11-10 20:38:59 -0300

    cambios en seeders y base de datos para que sea coherente con la tabla users de laravel (usada para login)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,34 @@ class DatabaseSeeder extends Seeder
             "name" => "usuario",
             "email" => "usuario@usuario"
         ]);
+        \App\Models\User::factory(1)->create([
+            "id" => "10",
+            "name"=>"toreto",
+        ]);
+        \App\Models\User::factory(1)->create([
+            "id" => "20",
+            "name"=>"choferpreba",
+        ]);
+        \App\Models\User::factory(1)->create([
+            "id" => "47",
+            "name"=>"usuario a listar",
+        ]);
+        \App\Models\User::factory(1)->create([
+            "id" => "42",
+            "name"=>"usuario a modificar",
+        ]);
+        \App\Models\User::factory(1)->create([
+            "id" => "74",
+            "name"=>"usuario a eliminar",
+        ]);
+        \App\Models\User::factory(1)->create([
+            "name"=>"admin",
+        ]);
+        \App\Models\User::factory(1)->create([
+            "name"=>"chofer",
+        ]);
+        \App\Models\User::factory(1)->create([
+            "name"=>"almacenero",
+        ]);
     }
 }
