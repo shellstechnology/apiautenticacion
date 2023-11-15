2ea004d38ddddd3ad9d14b3dcb7dd29bafb53e6f
AlejandroJMP24 <alejandromachado85@gmail.com>
2023-11-15 12:54:17 -0300

    Sistema que identifica de que aplicacion proviene el login implementado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Almaceneros;
+use App\Models\Choferes;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\User;
@@ -27,19 +29,38 @@ class UserController extends Controller
 
         return $this->crearUsuario($request);
     }
-    public function Login(Request $request){
-        if(Auth::attempt(['name' => $request->post('name'), 'password' => $request->post('password')])){ 
-            $user = Auth::user(); 
-            $success['token'] =  $user->createToken('authToken')-> accessToken; 
-            $success['name'] =  $user->name;
-            $success['id']=$user->id;
-            $response = response($success);
-            return $response ; 
-        } 
+    public function Login(Request $request)
+    {
+        if (Auth::attempt(['name' => $request->post('name'), 'password' => $request->post('password')])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('authToken')->accessToken;
+            $success['name'] = $user->name;
+            $success['id'] = $user->id;
+            $aplicacion = $this->respuestaAplicacion($request,$user);
+            if ($aplicacion == true) {
+                $response = response($success);
+                return $response;
+            }
+        }
 
-        return Response([ "message" => "Not authorized",], 401);
+        return Response(["message" => "Not authorized",], 401);
     }
 
+    public function respuestaAplicacion(Request $request,$user)
+    {
+        $datoAplicaicon = $request->post("aplicacion");
+        if ($datoAplicaicon == "choferes") {
+            $chofer=Choferes::where('id_usuarios',$user->id)->first();
+            if($chofer)
+            return true;
+        }
+        if($datoAplicaicon=='almacen'){
+            $almacen=Almaceneros::where('id_usuarios',$user->id)->first();
+            if($almacen)
+            return true;
+        }
+        return false;
+    }
     private function crearUsuario($request)
     {
         $user = new User();
---
+++ app/Models/choferes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class Choferes extends Model
+{
+    protected $table = 'choferes';
+    protected $primaryKey = 'id_usuarios';
+    protected $fillable =['id_usuarios','licencia_de_conducir'];
+    use HasFactory;
+    use SoftDeletes;
+    use ValidatesRequests;
+    public $timestamps = true;
+}
